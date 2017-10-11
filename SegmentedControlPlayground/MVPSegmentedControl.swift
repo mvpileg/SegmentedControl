231106edc1f8b2bba67658ca806d0248c56174b0
Matt Pileggi <mpileggi@swensonhe.com>
2017-10-10 19:00:37 -0700

    i hate this code

--- SegmentedControlPlayground/MVPSegmentedControl.swift
+++ SegmentedControlPlayground/MVPSegmentedControl.swift
@@ -24,17 +24,15 @@ class MVPSegmentedControl : UIView {
     private var optionViews: [UIView]?
     
     required init?(coder aDecoder: NSCoder) {
+        
+        //defaults
         selectedTextColor = UIColor.black
         defaultTextColor = UIColor.white
         selectedBackgroundColor = UIColor.orange
         
         super.init(coder: aDecoder)
         
-        layer.cornerRadius = 5
-        clipsToBounds = true
-        backgroundColor = UIColor.white.withAlphaComponent(0.1)
-        
-        self.options = ["Male", "Female"]
+        self.options = ["Low", "Medium", "High"]
         
         resetSubviews()
         applyDefaultStyles()
@@ -52,7 +50,8 @@ class MVPSegmentedControl : UIView {
         optionViews = []
 
         for i in 0..<options.count {
-            let subview = createSubview(withText: options[i])
+            let isLast = (i == options.count - 1)
+            let subview = createSubview(withText: options[i], withDivider: !isLast)
             let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
             subview.addGestureRecognizer(tap)
             
@@ -60,12 +59,14 @@ class MVPSegmentedControl : UIView {
 
             optionViews!.append(subview)
         }
-        
         addConstraints()
     }
     
     private func applyDefaultStyles() {
-
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+//        clipsToBounds = true
+        backgroundColor = UIColor.white.withAlphaComponent(0.1)
     }
     
     private func addConstraints() {
@@ -88,13 +89,13 @@ class MVPSegmentedControl : UIView {
         }
     }
     
-    private func createSubview(withText text: String) -> UIView {
+    private func createSubview(withText text: String, withDivider dividerVisible: Bool = true) -> UIView {
     
         //subview
         let subview = UIView()
         
         subview.translatesAutoresizingMaskIntoConstraints = false
-        subview.clipsToBounds = true
+        subview.layer.masksToBounds = true
         subview.layer.cornerRadius = 5
         
         //label
@@ -109,28 +110,58 @@ class MVPSegmentedControl : UIView {
         label.centerXAnchor.constraint(equalTo: subview.centerXAnchor).isActive = true
         label.centerYAnchor.constraint(equalTo: subview.centerYAnchor).isActive = true
 
+        //divider
+        if (dividerVisible) {
+            let divider = UIView()
+            divider.translatesAutoresizingMaskIntoConstraints = false
+            divider.backgroundColor = defaultTextColor
+            
+            subview.addSubview(divider)
+            
+            divider.trailingAnchor.constraint(equalTo: subview.trailingAnchor).isActive = true
+            divider.topAnchor.constraint(equalTo: subview.topAnchor, constant: 8.0).isActive = true
+            divider.bottomAnchor.constraint(equalTo: subview.bottomAnchor, constant: -8.0).isActive = true
+            divider.addConstraint(NSLayoutConstraint(item: divider, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 1.0))
+        }
+        
+
+        
+//        divider.widthAnchor.constraint(equalTo: NSLayoutAnchor.)
+        
+        
+        
         return subview
     }
 
     @objc private func handleTap(_ sender: UITapGestureRecognizer) {
         guard let optionsViews = optionViews else { return }
 
-        optionsViews.forEach { view in
+        for i in 0..<optionsViews.count {
+            let view = optionsViews[i]
             let selected = (view == sender.view)
-            let backgroundColor = selected ? selectedBackgroundColor : UIColor.clear
-            view.backgroundColor = backgroundColor
+            view.backgroundColor = selected ? selectedBackgroundColor : UIColor.clear
             
-            //TODO: refactor into uiview subclass that always has a label
-            view.subviews.forEach { possibleLabel in
-                if let label = possibleLabel as? UILabel {
+            
+            
+            //TODO: refactor into uiview subclass that always has a label and divider view
+            for i in 0..<view.subviews.count {
+                if let label = view.subviews[i] as? UILabel {
                     label.textColor = selected ? selectedTextColor : defaultTextColor
                 }
             }
         }
+        
+        for i in 0..<optionsViews.count-1 {
+            let dividerHidden = (optionsViews[i] == sender.view) || (optionsViews[i+1] == sender.view)
+           
+            for n in 0..<optionsViews[i].subviews.count {
+                if let label = optionsViews[i].subviews[n] as? UILabel {
+                    //do nothing because we write poopy code
+                } else {
+                    optionsViews[i].subviews[n].isHidden = dividerHidden
+                }
+            }
+        }
     }
-    
-
-    
-    
 }
 
